25cb44c923b4a11cc2bc51542a9e3e1afab065d4
Sameer <schauhan19@gmail.com>
2013-10-21 23:31:52 -0400

    Added menu. Work on splitting separating beginning of game.

--- classes.py
+++ classes.py
@@ -8,6 +8,59 @@ from random import choice
 from collections import defaultdict
 
 
+class Othello:
+    def __init__(self):
+        self.g = GUI()
+        self.b = Board()
+        self.showMenu()
+
+    def showMenu(self):
+        # input =
+        self.player1, self.player2 = self.g.getPlayer()
+        print("LET THE GAMES BEGIN!")
+        self.g.showBoard()
+
+    def play(self):
+        while True:
+        # Get valid black moves
+            validMoves = self.b.getValidMoves(self.player1)
+            self.g.updateBoard(self.b, self.player1)
+            # If there are any valid moves get Clicks
+            if validMoves:
+                gridXY = self.g.getClick()
+                # print(gridXY)
+                # If the click is in the valid --> Put tile, and let computer make moves
+                if gridXY in validMoves:
+                    self.b.putTile(gridXY, self.player1)
+                    self.g.updateBoard(self.b)
+                    pygame.time.wait(500)
+                    self.b.makeCompMove(self.player2)
+                    self.g.updateBoard(self.b)
+            else:
+                print("No valid Moves.. you skipped")
+                self.b.makeCompMove(self.player2)
+                self.g.updateBoard(self.b, self.player2)
+
+            # TODO Fix game flow. Possibly wrap game in another othello class
+
+
+            # Check end state
+            if self.b.checkEnd():
+                    print("Game ended")
+                    tileCount = self.b.getTileCount()
+                    print(tileCount)
+                    if tileCount[0] > tileCount[1]:
+                        print("Black wins")
+                    elif tileCount[0]< tileCount[1]:
+                        print("White wins")
+                    else:
+                        print("Magically a draw")
+                    pygame.time.wait(5000)
+                    pygame.quit()
+                    sys.exit()
+
+
+
 class Board:
     def __init__(self):
         self.board = np.zeros((8,8))
@@ -98,7 +151,6 @@ class Board:
 
         if n+rowI >7 or m+colI > 7 or n+rowI < 0 or m+colI< 0:
             return moveList
-        # TODO fix bug
         if n in range(8) and m in range(8) and self.board[n+rowI][m+colI] == other:
             n += rowI
             m += colI
@@ -110,7 +162,7 @@ class Board:
                     break
                 n += rowI
                 m += colI
-        return moveList #TODO fix edge case bugs
+        return moveList
 
     def flip(self,dir,gridXY,color):
 
@@ -185,9 +237,42 @@ class GUI:
         self.boardWidth = self.windowSize - 2*self.margin-10
 
         self.display = pygame.display.set_mode((self.windowSize,self.windowSize),0,32)
-        self.display.fill(BLUE)
+        self.display.fill(GREY)
         pygame.display.set_caption('Chocotaco Othello')
+
+    def showBoard(self):
+        self.display.fill(BLUE)
         pygame.draw.rect(self.display, BG, (self.margin+5, self.margin+5, self.boardWidth, self.boardWidth))
+
+    def getPlayer(self):
+        menu = Menu()
+        menu.init(['Black', 'White', 'Quit'], self.display)
+        menu.draw()
+        pygame.key.set_repeat(199,69)
+        pygame.display.update()
+        while 1:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        menu.draw(-1) #here is the Menu class function
+                    if event.key == K_DOWN:
+                        menu.draw(1) #here is the Menu class function
+                    if event.key == K_RETURN:
+                        if menu.get_position() ==0:
+                            return BLK, WHT
+                        if menu.get_position() ==1:
+                            return WHT, BLK
+                        elif menu.get_position() == 2:#here is the Menu class function
+                            pygame.display.quit()
+                            sys.exit()
+                    if event.key == K_ESCAPE:
+                        pygame.display.quit()
+                        sys.exit()
+                    pygame.display.update()
+                elif event.type == QUIT:
+                    pygame.display.quit()
+                    sys.exit()
+
 
     def updateBoard(self,boardClass, color=EMP):
         board = boardClass.board
@@ -218,3 +303,95 @@ class GUI:
                     yPos = (mouseY - self.margin)/self.spaceSize
                     return yPos, xPos
 
+
+
+class Menu:
+    lista = []
+    pola = []
+    rozmiar_fontu = 32
+    font_path = 'font/coders_crux.ttf'
+    font = pygame.font.Font
+    dest_surface = pygame.Surface
+    ilosc_pol = 0
+    kolor_tla = (51,51,51)
+    kolor_tekstu =  (255, 255, 153)
+    kolor_zaznaczenia = (153,102,255)
+    pozycja_zaznaczenia = 0
+    pozycja_wklejenia = (0,0)
+    menu_width = 0
+    menu_height = 0
+
+    class Pole:
+        tekst = ''
+        pole = pygame.Surface
+        pole_rect = pygame.Rect
+        zaznaczenie_rect = pygame.Rect
+
+    def move_menu(self, top, left):
+        self.pozycja_wklejenia = (top,left)
+
+    def set_colors(self, text, selection, background):
+        self.kolor_tla = background
+        self.kolor_tekstu =  text
+        self.kolor_zaznaczenia = selection
+
+    def set_fontsize(self,font_size):
+        self.rozmiar_fontu = font_size
+
+    def set_font(self, path):
+        self.font_path = path
+
+    def get_position(self):
+        return self.pozycja_zaznaczenia
+
+    def init(self, lista, dest_surface):
+        self.lista = lista
+        self.dest_surface = dest_surface
+        self.ilosc_pol = len(self.lista)
+        self.stworz_strukture()
+
+    def draw(self,przesun=0):
+        if przesun:
+            self.pozycja_zaznaczenia += przesun
+            if self.pozycja_zaznaczenia == -1:
+                self.pozycja_zaznaczenia = self.ilosc_pol - 1
+            self.pozycja_zaznaczenia %= self.ilosc_pol
+        menu = pygame.Surface((self.menu_width, self.menu_height))
+        menu.fill(self.kolor_tla)
+        zaznaczenie_rect = self.pola[self.pozycja_zaznaczenia].zaznaczenie_rect
+        pygame.draw.rect(menu,self.kolor_zaznaczenia,zaznaczenie_rect)
+
+        for i in xrange(self.ilosc_pol):
+            menu.blit(self.pola[i].pole,self.pola[i].pole_rect)
+        self.dest_surface.blit(menu,self.pozycja_wklejenia)
+        return self.pozycja_zaznaczenia
+
+    def stworz_strukture(self):
+        przesuniecie = 0
+        self.menu_height = 0
+        self.font = pygame.font.Font(self.font_path, self.rozmiar_fontu)
+        for i in xrange(self.ilosc_pol):
+            self.pola.append(self.Pole())
+            self.pola[i].tekst = self.lista[i]
+            self.pola[i].pole = self.font.render(self.pola[i].tekst, 1, self.kolor_tekstu)
+
+            self.pola[i].pole_rect = self.pola[i].pole.get_rect()
+            przesuniecie = int(self.rozmiar_fontu * 0.2)
+
+            height = self.pola[i].pole_rect.height
+            self.pola[i].pole_rect.left = przesuniecie
+            self.pola[i].pole_rect.top = przesuniecie+(przesuniecie*2+height)*i
+
+            width = self.pola[i].pole_rect.width+przesuniecie*2
+            height = self.pola[i].pole_rect.height+przesuniecie*2
+            left = self.pola[i].pole_rect.left-przesuniecie
+            top = self.pola[i].pole_rect.top-przesuniecie
+
+            self.pola[i].zaznaczenie_rect = (left,top ,width, height)
+            if width > self.menu_width:
+                    self.menu_width = width
+            self.menu_height += height
+        x = self.dest_surface.get_rect().centerx - self.menu_width / 2
+        y = self.dest_surface.get_rect().centery - self.menu_height / 2
+        mx, my = self.pozycja_wklejenia
+        self.pozycja_wklejenia = (x+mx, y+my)
--- main.py
+++ main.py
@@ -5,57 +5,18 @@ from classes import *
 from const import *
 
 
-def playGame():
-    while True: # Game Loop
-        player = raw_input("Black or white? (b/w)")
-
-
 def testGame():
     b = Board()
     g = GUI()
 
     # playGame()
-    while True:
-        # Get valid black moves
-        validMoves = b.getValidMoves(BLK)
-        g.updateBoard(b, BLK)
-        # If there are any valid moves get Clicks
-        if validMoves:
-            gridXY = g.getClick()
-            # print(gridXY)
-            # If the click is in the valid --> Put tile, and let computer make moves
-            if gridXY in validMoves:
-                b.putTile(gridXY, BLK)
-                g.updateBoard(b)
-                pygame.time.wait(500)
-                b.makeCompMove(WHT)
-                g.updateBoard(b)
-        else:
-            print("No valid Moves.. you skipped")
-            b.makeCompMove(WHT)
-            g.updateBoard(b,WHT)
-
-        # TODO Fix game flow. Possibly wrap game in another othello class
 
 
-        # Check end state
-        if b.checkEnd():
-                print("Game ended")
-                tileCount = b.getTileCount()
-                print(tileCount)
-                if tileCount[0] > tileCount[1]:
-                    print("Black wins")
-                elif tileCount[0]< tileCount[1]:
-                    print("White wins")
-                else:
-                    print("Magically a draw")
-                pygame.time.wait(5000)
-                pygame.quit()
-                sys.exit()
-
-    #TODO apply checks before placing tile Call update board after moves (flips included)
+def main():
+    othello = Othello()
+    othello.play()
 
 if __name__ == "__main__" :
-    testGame()
+    main()
 
 
